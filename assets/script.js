6f61cf9f95460bf7f0bd88807382f5f0d0c0178e
wyatt otto <wyattotto87@gmail.com>
2023-02-22 17:16:42 -0500

    worked on classes getting added by time on the planner

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,23 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var dateDisplayEL =$('#currentDay')
 var saveBtnEL = $('.saveBtn')
-$(function () {
+
+
+
+
+
+$(document).ready(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  saveBtnEL.on('click', function(){
+  
     
-  })
+  // })
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
@@ -23,4 +29,35 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  var todayDate = dayjs().format('MMMM DD YYYY')
+  dateDisplayEL.text(todayDate);
+
+  
+  var now = dayjs().hour()
+  console.log(now)
+  // var hours = now.getHours()
+  
+  const eventElements = document.querySelectorAll (".time-block") 
+  // for (let i = 0; i < eventElements.length; i++){
+  //   const eventEl = eventElements[i];
+  $(".time-block").each (function updaterMethod(){
+    const eventHour = parseInt ($(this).attr("id").split("-")[1])
+    if (eventHour < now){
+    $(this).removeClass("present future").addClass("past") 
+    }
+    else if (eventHour === now){
+      $(this).removeClass("past future").addClass("present")
+    }
+    else {
+    $(this).removeClass("past present").addClass("future")
+    }
+
+  })
+   updaterMethod() 
+    
+
 });
+
+
+
+
