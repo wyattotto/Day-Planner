24254470f3e286bc38372de196b2f47964931152
wyatt otto <wyattotto87@gmail.com>
2023-02-25 19:35:23 -0500

    Added save button function to save txt to local storage

--- assets/script.js
+++ assets/script.js
@@ -1,92 +1,39 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-var dateDisplayEL =$('#currentDay')
- var saveBtnEL =$('#saveBtn')
- var now = dayjs().hour()
-
-
-
-
+var dateDisplayEL = $("#currentDay");
 
 $(document).ready(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  // loadData()
-  // function loadData()
-  // $(".description").each.html(localStorage.myData)
-  
-  // $("saveBtnEL").click(function(){
-  //   var disTxt = $("text").val()
-  //   var data = <p> + text + </p>
+  var saveBtnEL = $(".saveBtn");
+  $(".description").each(function () {
+    this.value=localStorage.getItem($(this).parent().attr("id"));
+  });
 
-  //   localStorage.myData=data
-  // })
-  // })
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-  var todayDate = dayjs().format('MMMM DD YYYY')
-  dateDisplayEL.text(todayDate);
-
-  
-  var now = dayjs().hour()
-  console.log(now)
-  // var hours = now.getHours()
-  
-  const eventElements = document.querySelectorAll (".time-block") 
-  // for (let i = 0; i < eventElements.length; i++){
-  //   const eventEl = eventElements[i];
-  // $(".time-block").each (function updaterMethod(){
-  //   const eventHour = parseInt ($(this).attr("id").split("-")[1])
-  //   if (eventHour < now){
-  //   $(this).removeClass("present future").addClass("past") 
-  //   }
-  //   else if (eventHour === now){
-  //     $(this).removeClass("past future").addClass("present")
-  //   }
-  //   else {
-  //   $(this).removeClass("past present").addClass("future")
-  //   }
-  //   // setInterval(updaterMethod, 1000)
-  // })
-  
-  
-    
-    
+  saveBtnEL.click(function () {
+    localStorage.setItem(
+      $(this).parent().attr("id"),
+      $(this).siblings("textarea").val()
+    );
+  });
 
 });
 
+var todayDate = dayjs().format("MMMM DD YYYY");
+dateDisplayEL.text(todayDate);
 
-function time (){
 
-$(".time-block").each (function updaterMethod(){
-  const eventHour = parseInt ($(this).attr("id").split("-")[1])
-  if (eventHour < now){
-  $(this).removeClass("present future").addClass("past") 
-  }
-  else if (eventHour === now){
-    $(this).removeClass("past future").addClass("present")
-  }
-  else {
-  $(this).removeClass("past present").addClass("future")
-  }
-  
-})
+
+const eventElements = document.querySelectorAll(".time-block");
+function time() {
+  var now = dayjs().hour();
+  $(".time-block").each(function updaterMethod() {
+    const eventHour = parseInt($(this).attr("id").split("-")[1]);
+    if (eventHour < now) {
+      $(this).removeClass("present future").addClass("past");
+    } else if (eventHour === now) {
+      $(this).removeClass("past future").addClass("present");
+    } else {
+      $(this).removeClass("past present").addClass("future");
+    }
+  });
 }
-setInterval(time, 1000)
 
-
-
+time()
+setInterval(time, 1000);
